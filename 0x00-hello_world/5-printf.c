e63559a26138dd7f034fcc5f943b1965cd4e204f
Eliane-M <munezeroeliane761@gmail.com>
2023-05-29 13:08:21 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print a line of words using printf
+ * created by Eliane
+ * Return:0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
