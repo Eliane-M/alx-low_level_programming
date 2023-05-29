35e7b2e4616f6ba700850f1c7516780317a08ca5
Eliane-M <munezeroeliane761@gmail.com>
2023-05-29 15:15:07 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "reset.h"
+/**
+ * reset_to_98 - write a function to reset a pointer
+ * created by Eliane
+ * @n: type integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/reset.h
@@ -0,0 +1,4 @@
+#ifndef RESET_H
+#define RESET_H
+void reset_to_98(int *n);
+#endif 
