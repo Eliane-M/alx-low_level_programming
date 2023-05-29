17132f90f5fa150032b991725b73bfd493699234
Eliane-M <munezeroeliane761@gmail.com>
2023-05-29 12:57:26 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0
+ */
+int main(void)
+{
+	char p[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(p);
+	return (0);
+}
